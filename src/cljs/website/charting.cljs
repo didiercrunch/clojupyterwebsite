ffb106808bb826eccb1f0f9e63fac1c30e791992
didier amyot <didier.amyot@gmail.com>
2018-02-03 12:39:05 -0500

    worked on the UI

---
+++ src/cljs/website/charting.cljs
@@ -0,0 +1,5 @@
+(ns website.charting)
+
+
+(defn charting-page []
+  [:h1 "hello world | charting page"])
--- src/cljs/website/tobbar.cljs
+++ src/cljs/website/tobbar.cljs
@@ -1,16 +1,29 @@
 (ns website.tobbar
    (:require [website.logo :refer [logo]]))
 
+
+(defn important [w] ;; to remove
+  [:span.has-text-weight-semibold w])
+
 (def github-link
   [:a.title.is-5 {:href "https://github.com/clojupyter"}
     [:i.fab.fa-github]])
 
-
 (defn topbar []
    [:nav.navbar.is-primary.is-transparent {:role "navigation" :aria-label "main navigation"}
     [:div.navbar-brand
-     [:a.navbar-item
-      [logo 30]]]
+     [:a.navbar-item {:href "/"}
+      [logo 30]]
+     [:div.navbar-burger
+      [:div.navbar-burger
+       [:span]
+       [:span]
+       [:span]]]]
     [:div.navbar-menu
+     [:div.navbar-start
+      [:a.navbar-item {:href "/charting" :style {:color "white"}}
+       (important [:p [:i.fas.fa-chart-bar ] " Charting"])]
+      [:a.navbar-item {:href "/install" :style {:color "white"}}
+       (important [:p [:i.fas.fa-arrow-down ] " Install"])]]
      [:div.navbar-end
       [:a.navbar-item github-link]]]])
